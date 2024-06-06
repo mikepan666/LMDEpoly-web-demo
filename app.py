b945548caf444d8fac0b58c9aa9d304f8183f6ce
mikepan666 <130286391+mikepan666@users.noreply.github.com>
2024-06-06 16:23:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import gradio as gr
 import os
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
+pip install sentencepiece
+
 
 # download internlm2 to the base_path directory using git tool
 base_path = './internlm2-chat-1_8B'
